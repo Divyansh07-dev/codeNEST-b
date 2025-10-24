5e8bf3ed97ca810df7b2c15cf2fac77175f9a752
Divyansh <divyanshsharma1972005@gmail.com>
2025-10-24 23:08:48 +0530

    FIX: Made the /getAllProblem route public (unauthenticated).

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const problemRouter =  express.Router();
+const problemRouter = express.Router();
 const adminMiddleware = require("../middleware/adminMiddleware");
 const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblembyUser,submittedProblem} = require("../controllers/userProblem");
 const userMiddleware = require("../middleware/userMiddleware");
@@ -12,14 +12,13 @@ problemRouter.put("/update/:id",adminMiddleware, updateProblem);
 problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
 
 
+// 🛑 FINAL FIX: Remove userMiddleware so the problems list is public
+problemRouter.get("/getAllProblem", getAllProblem);
+
+// The remaining routes still require user authentication
 problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
-problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
 problemRouter.get("/problemSolvedByUser",userMiddleware, solvedAllProblembyUser);
 problemRouter.get("/submittedProblem/:pid",userMiddleware,submittedProblem);
 
 
 module.exports = problemRouter;
-
-// fetch
-// update
-// delete 
